3f2fae268d7c394f5cef30d0d34198a7e8f27809
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-04-29 19:35:52 +0530

    Update url_uploader.py

--- url_uploader.py
+++ url_uploader.py
@@ -3,6 +3,19 @@ import requests
 from tqdm import tqdm
 from telethon.sync import TelegramClient
 from dotenv import load_dotenv
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive", 200
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 load_dotenv()  # Load environment variables from .env
 
